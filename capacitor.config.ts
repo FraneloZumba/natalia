7b5d622682fdd738e57337dc0471a36a9023b88c
rafrazumba <rafrazumba@gmail.com>
2025-05-04 19:08:18 +0000

    paraminat

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,8 +2,8 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.nat.app',
-  appName: 'myapp',
-  webDir: 'dist/myapp'
+  appName: 'Para mi Nat',
+  webDir: 'dist/myapp/browser'
 };
 
 export default config;
